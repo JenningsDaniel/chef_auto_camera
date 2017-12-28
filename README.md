9e057600d0f92ea10ec216425fdc6b79e7d7658c
Daniel Jennings <daniel.j.jennings@gmail.com>
2017-12-28 13:46:39 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# auto_camera
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: auto_camera
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
